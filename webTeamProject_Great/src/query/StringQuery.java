2ded20c3f502e13d7fe5eafa98ab30e3e25abf53
habaree <haha007164@gmail.com>
2018-10-02 10:27:12 +0900

    pagingbean stringquery add

---
+++ webTeamProject_Great/src/query/StringQuery.java
@@ -0,0 +1,14 @@
+package query;
+
+import model.dao.CommonConstants;
+
+public interface StringQuery {
+
+	String NOTICE = "SELECT no, writer, img_urls, register_date, content FROM notice";
+	
+	String PAGE_LIST = "SELECT no, writer, img_urls,register_date, content" +
+            "(SELECT no, writer, img_urls,register_date, content, ceil(rownum/"+CommonConstants.CONTENT_NUMBER_PER_PAGE+") AS page FROM" +
+            "(SELECT no, writer, img_urls,to_char(time_posted, 'YYYY.MM.DD') register_date, content FROM board order by no desc)) where page=?";
+
+	String TOTAL_COUNT = "select count(-1) from board";
+}
